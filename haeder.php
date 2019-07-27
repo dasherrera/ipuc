4e1ffa9646b30314d059ab37875fe8dba5fafceb
dasherrer <dsherrera39@misena.edu.co>
2019-07-27 15:48:43 -0500

    confirmando cambios

---
+++ haeder.php
@@ -0,0 +1,50 @@
+<?php 
+//include 'conexion/conexion.php';
+//$conn=mysql_query("Select TituloPagP from titulos");
+//if ($conn!=0) {
+//	$titulo=conn;
+//	echo "<title>"+$titulo+"</title";
+//}elseif ($conn<=0 ) {
+//	echo "<title>pagina sin titulo</title";
+//}
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>	
+<link rel="stylesheet" type="text/css" href="css/header.css">
+</head>
+<body>
+	<header>
+		<div id="Container">
+			<div class="Container">
+				<ul>
+					<li class="item1">
+						<a href="#">Home</a>
+					</li>
+
+					<li class="item2">
+						<a href="#">Acerca de Nosotros</a>
+					</li>
+
+					<li class="item3">
+						<a href="#">Predicaciones</a>
+					</li>
+
+					<li class="item4">
+						<a href="#">Musica</a>
+					</li>
+
+					<li class="item5">
+						<a href="#">Noticias</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</header>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+include 'conexion/conexion.php';
+include 'haeder.php';
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+	<title>Radio Vida</title>
+</head>
+<body>
+
+</body>
+</html>
